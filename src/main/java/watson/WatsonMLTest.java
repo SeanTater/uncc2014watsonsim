6f8e7ebb5855aaf71e02fc7bb384dad16d367789
Sean Gallagher <stgallag@gmail.com>
2014-01-20 21:20:50 -0500

    Create some tests for watson (not passing yet)

---
+++ src/main/java/watson/WatsonMLTest.java
@@ -0,0 +1,25 @@
+package watson;
+
+import static org.junit.Assert.*;
+import java.util.*;
+import org.junit.Test;
+
+public class WatsonMLTest {
+
+	@Test
+	public void testAggregate() {
+		// Setup possible inputs
+		Resultset yahoo = new Resultset("Yahoo");
+		yahoo.add(new Result("Alligators", "doc1", "They're Aunt Annie's", 0.25));
+		Resultset bing = new Resultset("Bing");
+		bing.add(new Result("Alligators", "doc1", "They're Aunt Annie's", 0.75));
+		
+		// Make an exact copy when there is 1 result
+		Resultset output_set;
+		
+		WatsonML ml = new WatsonML();
+		output_set = ml.aggregate(yahoo);
+		assertEquals(output_set.get(0), yahoo.get(0));
+	}
+
+}
