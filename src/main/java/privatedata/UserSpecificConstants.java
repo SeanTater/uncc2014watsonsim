818e693da1f9c533da792fbd1a6c9fe873f6439a
Sean Gallagher <stgallag@gmail.com>
2014-02-25 06:47:32 -0500

    Give more example user constants

--- src/main/java/privatedata/UserSpecificConstants.java
+++ src/main/java/privatedata/UserSpecificConstants.java
@@ -5,4 +5,8 @@ public class PrivateGoogleCredentials {
 	public static final String googleApplicationName = "UNCC Watson Clone";
 	public static final String googleAPIKey = "AIzaSyAMF-XnRYf-A_Fp8s_Fn-orYkUwgv6JDW0"; //Google provided API key
 	public static final String googleCustomSearchID = "015506977713410602215:ykifjq-bmuq";
+	
+	public static final String indriIndex = "/example/indri/index";
+        public static final String luceneIndex = "/example/lucene/index";
+        public static final String luceneSearchField = "text";
 }
