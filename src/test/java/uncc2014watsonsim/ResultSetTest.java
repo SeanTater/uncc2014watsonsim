db151bfcbe3c362d44aafc88981fa302cbe67661
Sean Gallagher <stgallag@gmail.com>
2014-03-01 14:52:58 -0500

    fix ResultSet tests

--- src/test/java/uncc2014watsonsim/ResultSetTest.java
+++ src/test/java/uncc2014watsonsim/ResultSetTest.java
@@ -12,12 +12,12 @@ public class ResultSetTest {
 	public void testEqualsObject() {
 		// Results are equal if their titles are similar.
 		assertEquals(
-			new ResultSet("duck duck goose", "example", 0, 0, false),
-			new ResultSet("duck duck", "example", 0, 0, false));
+			new ResultSet("duck duck goose", "text", "engine", 0, 0, false),
+			new ResultSet("duck duck", "text", "engine", 0, 0, false));
 
 		assertEquals(
-			new ResultSet("Pete and repeat were sitting on a fence", "example", 0, 0, false),
-			new ResultSet("Peteand repeat were sitting on a fence", "example", 0, 0, false));
+			new ResultSet("Pete and repeat were sitting on a fence", "text", "engine", 0, 0, false),
+			new ResultSet("Peteand repeat were sitting on a fence", "text", "engine", 0, 0, false));
 	}
 
 }
--- src/test/java/uncc2014watsonsim/ScorerTest.java
+++ src/test/java/uncc2014watsonsim/ScorerTest.java
@@ -19,10 +19,10 @@ public class ScorerTest {
 	@Before
 	public void setUp() {
 		// Setup possible inputs
-		yahoo1 = new ResultSet("Alligators", "yahoo", 1, 0.75, true);
-		yahoo2 = new ResultSet("Eels", "yahoo", 2, 0.38, false);
-		bing1 = new ResultSet("Alligators", "bing", 1, 0.25, true);
-		bing2 = new ResultSet("Elk", "bing", 2, 0.19, false);
+		yahoo1 = new ResultSet("Alligators", "text", "yahoo", 1, 0.75, true);
+		yahoo2 = new ResultSet("Eels", "text", "yahoo", 2, 0.38, false);
+		bing1 = new ResultSet("Alligators", "text", "bing", 1, 0.25, true);
+		bing2 = new ResultSet("Elk", "text", "bing", 2, 0.19, false);
 		ml = new AverageScorer();
 	}
 
