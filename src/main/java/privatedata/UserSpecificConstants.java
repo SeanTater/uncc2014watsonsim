2a16a33e158c7782a54ce51ac1e5119546c97d63
Jagan Vujjini <jagan120@gmail.com>
2014-03-13 12:56:15 -0400

    changes to IGNORE

--- src/main/java/privatedata/UserSpecificConstants.java
+++ src/main/java/privatedata/UserSpecificConstants.java
@@ -2,14 +2,14 @@ package privatedata;
 
 public class UserSpecificConstants {
 	// Constants
-	public static final String googleApplicationName = "ITCS4010 Google";
-	public static final String googleAPIKey = "AIzaSyB3-c2634TACDUJ2L6Z2YZw54YaOTkpwxY"; //Google provided API key
-	public static final String googleCustomSearchID = "011139517373112470727:irjnobrmjre";
+	public static final String googleApplicationName = "JWebPaidSearch";
+	public static final String googleAPIKey = "AIzaSyCKTOWQ7_frEn3dvqdQRnbZ6fhvo7F-WG4"; //Google provided API key
+	public static final String googleCustomSearchID = "008991626419004307712:atwudspyacm";
 	
 	public static final String indriIndex = "/home/jvujjini/Watson/wiki_indri_index";
     public static final String luceneIndex = "/home/jvujjini/Watson/wiki_lucene_index";
-    public static final String quotesIndriIndex = "/home/jvujjini/Watson/wiki_indri_index";
-    public static final String quotesLuceneIndex = "/home/jvujjini/Watson/wiki_lucene_index";
+    public static final String quotesIndriIndex = "/home/jvujjini/Watson/quotes_indri_index";
+    public static final String quotesLuceneIndex = "/home/jvujjini/Watson/quotes_lucene_index";
     public static final String luceneSearchField = "text";
     public static final String indriResultsFilter = "#filrej(list.title #combine(%s))"; 
     public static final String luceneResultsFilter = " NOT title:*\\:*" + " NOT title:list*";
