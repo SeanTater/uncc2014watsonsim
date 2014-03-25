e86f7e8d7d2c34d71217029f2cda5da5976b828e
Jagan Vujjini <jagan120@gmail.com>
2014-03-25 12:12:57 -0400

    UserSpecs Changed

--- src/main/java/privatedata/UserSpecificConstants.java
+++ src/main/java/privatedata/UserSpecificConstants.java
@@ -2,12 +2,12 @@ package privatedata;
 
 public class UserSpecificConstants {
 	// Constants
-	public static final String googleApplicationName = "WatsonDev2";
-	public static final String googleAPIKey = "AIzaSyBSlx_FfK2IvqLyggfjKcMAT52zwEvzmls"; //Google provided API key
-	public static final String googleCustomSearchID = "010178321218681199181:bqf2nn4mb2i";
+	public static final String googleApplicationName = "";
+	public static final String googleAPIKey = ""; //Google provided API key
+	public static final String googleCustomSearchID = "";
 	
-	public static final String indriIndex = "/home/jvujjini/Watson/wiki_indri_index";
-	public static final String luceneIndex = "/home/jvujjini/Watson/wiki_lucene_index";
+	public static final String indriIndex = "wiki_indri_index";
+	public static final String luceneIndex = "wiki_lucene_index";
 	public static final String luceneSearchField = "text";
 	public static final String indriResultsFilter = "#filrej(list.title #combine(%s))"; 
 	public static final String luceneResultsFilter = " NOT title:*\\:*" + " NOT title:list*";
