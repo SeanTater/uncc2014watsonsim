61d0f8a500d1b91c45b8431ee0aaf5749c84ee84
Sean Gallagher <stgallag@gmail.com>
2014-03-27 20:46:07 -0400

    Bing -> merged

--- src/main/java/uncc2014watsonsim/search/BingSearcher.java
+++ src/main/java/uncc2014watsonsim/search/BingSearcher.java
@@ -1,21 +1,15 @@
 package uncc2014watsonsim.search;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import privatedata.UserSpecificConstants;
-import org.apache.http.client.fluent.*;
-import org.apache.commons.codec.binary.Base64;
-//import org.jsoup.Jsoup;
 
+import org.apache.http.client.fluent.*;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import uncc2014watsonsim.Answer;
 		
@@ -25,106 +19,35 @@ public class BingSearcher extends Searcher {
 		
 		//TODO: Should this be done in StringUtils?
 	    query = query.replaceAll(" ", "%20");
-	    String url;
+	    String url = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Web?Query=%27" + query + "%27&$top=50&$format=Atom";
+
+	    List<Answer> results = new ArrayList<Answer>();
 	    try {
-	    	url = "https://api.datamarket.azure.com/Data.ashx/Bing/Search/v1/Web?Query=%27" + query + "%27&$top=50&$format=Atom";
 	    	String resp = Executor
 	    		.newInstance()
 	    		.auth(UserSpecificConstants.bingAPIKey, UserSpecificConstants.bingAPIKey)
 	    		.execute(Request.Get(url))
-	    		.returnContent()
-	    		.asString();
-		    // ?? conn.setRequestProperty("Accept", "application/json");
+	    		.returnContent().asString();
 	    	
-		    int count = 0;
-		    List<String> Titles = new ArrayList<String>();
-		    List<String> Descriptions = new ArrayList<String>();
-		    List<String> Urls = new ArrayList<String>();
-		    
-		  //*************************Title*******************************		
-		    List<Integer> startPositions = new ArrayList<Integer>();
-		    Pattern ps = Pattern.compile("<d:Title");
-		    Matcher ms = ps.matcher(resp);
-		    while (ms.find() && count < MAX_RESULTS) {
-		    	startPositions.add(ms.start());
-		    	count++;
-		    }
-		    
-		    count = 0;
-		    List<Integer> endPositions = new ArrayList<Integer>();
-		    Pattern pe = Pattern.compile("</d:Title>");
-		    Matcher me = pe.matcher(resp);
-		    while (me.find() && count < MAX_RESULTS) {
-		    	endPositions.add(me.start());
-		        count++;
-		    }
-		    
-		    for (int x = 0; x < MAX_RESULTS; x++)
-		    {
-		    	Titles.add(resp.substring(startPositions.get(x)+29, endPositions.get(x)));
-		    }
-		    
-		  //*************************Description*******************************		
-		    count = 0;
-		    startPositions = new ArrayList<Integer>();
-		    ps = Pattern.compile("<d:Description");
-		    ms = ps.matcher(resp);
-		    while (ms.find() && count < MAX_RESULTS) {
-		    	startPositions.add(ms.start());
-		    	count++;
-		    }
-		    
-		    count = 0;
-		    endPositions = new ArrayList<Integer>();
-		    pe = Pattern.compile("</d:Description>");
-		    me = pe.matcher(resp);
-		    while (me.find() && count < MAX_RESULTS) {
-		    	endPositions.add(me.start());
-		        count++;
-		    }
-		    
-		    for (int x = 0; x < MAX_RESULTS; x++)
-		    {
-		    	Descriptions.add(resp.substring(startPositions.get(x)+35, endPositions.get(x)));
-		    }
-		    
-		  //*************************URL*******************************		
-		    count = 0;
-		    startPositions = new ArrayList<Integer>();
-		    ps = Pattern.compile("<d:Url");
-		    ms = ps.matcher(resp);
-		    while (ms.find() && count < MAX_RESULTS) {
-		    	startPositions.add(ms.start());
-		    	count++;
-		    }
-		    
-		    count = 0;
-		    endPositions = new ArrayList<Integer>();
-		    pe = Pattern.compile("</d:Url>");
-		    me = pe.matcher(resp);
-		    while (me.find() && count < MAX_RESULTS) {
-		    	endPositions.add(me.start());
-		        count++;
-		    }
-		    
-		    for (int x = 0; x < MAX_RESULTS; x++)
-		    {
-		    	Urls.add(resp.substring(startPositions.get(x)+27, endPositions.get(x)));
-		    }
-		    
-		    for(int x = 0; x < Titles.size(); x++)
-		    {
-		    	System.out.println("Rank " + (x+1));
-		    	System.out.println("Title: " + Titles.get(x));
-		    	System.out.println("Description: " + Descriptions.get(x));
-		    	System.out.println("Url: " + Urls.get(x));
-		    	System.out.println();
-		    }
+	    	Document doc = Jsoup.parse(resp);
+	    	
+		    int i=0;
+	    	for (Element e : doc.select("entry")) {
+	    		results.add(new Answer(
+	        			"bing",         	// Engine
+	        			e.select("d|Title").text(),	        // Title
+	        			e.select("d|Description").text(), // Full Text
+	        			e.select("d|Url").text(),          // Reference
+	    				i,                  // Rank
+	    				0		// Score
+	    			));
+	    		i++;
+	    	}
 		    
 	    } catch (IOException e) {
 	    	System.out.println("Error while searching with Bing. Ignoring. Details follow.");
 	        e.printStackTrace();
 	    }
-	    return new ArrayList<Answer>();
+	    return results;
 	}
 }
