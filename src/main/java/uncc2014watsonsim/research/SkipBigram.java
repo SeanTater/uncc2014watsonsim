cf8eecd9f2de186d6e6b26396bf4290b990e4173
Sean Gallagher <stgallag@gmail.com>
2014-04-08 11:14:57 -0400

    Add hyphen trimmer and sample skip bigramn

---
+++ src/main/java/uncc2014watsonsim/research/SkipBigram.java
@@ -0,0 +1,13 @@
+package uncc2014watsonsim.research;
+
+import uncc2014watsonsim.Answer;
+import uncc2014watsonsim.Passage;
+import uncc2014watsonsim.Question;
+
+public class SkipBigram extends Scorer {
+	
+	public double passage(Question q, Answer a, Passage p) {
+		return Double.NaN;
+	}
+
+}
