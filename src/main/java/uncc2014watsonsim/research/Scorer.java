e3bb3fd0bb3cf6f5fbcedbf8bbb8d34a6b38f543
Ken Overholt <kroverho@uncc.edu>
2014-04-11 08:05:39 -0400

    Removed invalid PassageScore import

--- src/main/java/uncc2014watsonsim/research/Scorer.java
+++ src/main/java/uncc2014watsonsim/research/Scorer.java
@@ -7,7 +7,6 @@ import java.util.concurrent.RecursiveTask;
 
 import uncc2014watsonsim.Answer;
 import uncc2014watsonsim.Passage;
-import uncc2014watsonsim.PassageScore;
 import uncc2014watsonsim.Question;
 import uncc2014watsonsim.Score;
 
