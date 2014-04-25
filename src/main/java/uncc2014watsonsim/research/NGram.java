b716bc5eddbb46fccef84e880c424ac4b903dad7
Sean Gallagher <stgallag@gmail.com>
2014-04-25 10:29:29 -0400

    Ngram speedup

--- src/main/java/uncc2014watsonsim/research/NGram.java
+++ src/main/java/uncc2014watsonsim/research/NGram.java
@@ -17,30 +17,27 @@ import uncc2014watsonsim.StringUtils;
 public class NGram extends PassageScorer {
 	public double scorePassage(Question q, Answer a, Passage p) {
 		// Jane Austen
-				ArrayList<String> a_set = generateNgrams(3,StringUtils.filterRelevant(a.candidate_text));
-				
-				// Romantic novelist Jane Austen once wrote -the- book Emma.
-				ArrayList<String> p_set = generateNgrams(3,StringUtils.filterRelevant(p.text));
-				
-				
-				
-				a_set.retainAll(p_set);
-				
-				return a_set.size();
+		List<String> a_set = generateNgrams(3, a.candidate_text);
+		
+		// Romantic novelist Jane Austen once wrote -the- book Emma.
+		List<String> p_set = generateNgrams(3, p.text);
+		
+		a_set.retainAll(p_set);
+		return a_set.size();
 			
 	}
-	public static ArrayList<String> generateNgrams(int n, String str) {
-        ArrayList<String> ngrams = new ArrayList<String>();
-        String[] words = str.split(" ");
-        for (int i = 0; i < words.length - n + 1; i++)
+	public static List<String> generateNgrams(int n, String str) {
+		List<String> words = StringUtils.tokenize(str);
+        List<String> ngrams = new ArrayList<String>();
+        for (int i = 0; i < words.size() - n + 1; i++)
             ngrams.add(concat(words, i, i+n));
         return ngrams;
 	}
 	
-	public static String concat(String[] words, int start, int end) {
+	public static String concat(List<String> words, int start, int end) {
         StringBuilder sb = new StringBuilder();
         for (int i = start; i < end; i++)
-            sb.append((i > start ? " " : "") + words[i]);
+            sb.append((i > start ? " " : "") + words.get(i));
         return sb.toString();
     }
 	
