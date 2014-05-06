775b44bd1f134c689a65390824248635604f2d89
csteph16 <csteph16@uncc.edu>
2014-05-06 12:22:47 -0400

    Update StephensonOpenNLPScorer.java

--- src/main/java/uncc2014watsonsim/scorers/StephensonOpenNLPScorer.java
+++ src/main/java/uncc2014watsonsim/scorers/StephensonOpenNLPScorer.java
@@ -27,5 +27,57 @@ import org.apache.commons.lang3.StringUtils;
  */
 
 public class StephensonOpenNLPScorer {
+  private boolean modelsAreInitialized=false;	
+
+	public  String modelsPath="data/"; //models directory
+	private File parserMFile; 
+	private File sentDetectorMFile;
+	private File chunkerMFile;
+	private File posMFile;
+
+	public SentenceModel sentenceModel; //sentence detection model 
+	public ParserModel parserModel; //parsing model
+	public POSTaggerME tagger;
+
+
+	public void init() throws InvalidFormatException{
+		File modelsDir = new File(this.modelsPath);
+
+		this.parserMFile = new File(modelsDir, "en-parser-chunking.bin");
+		this.sentDetectorMFile = new File(modelsDir, "en-sent.bin");
+		this.chunkerMFile=new File(modelsDir,"en-chunker.bin");
+		this.posMFile = new File(modelsDir,"en-pos-maxent.bin");
+
+		InputStream sentModelIn = null;
+		FileInputStream parserStream;
+		try {
+			//for finding sentences
+			sentModelIn = new FileInputStream(sentDetectorMFile);
+			this.sentenceModel = new SentenceModel(sentModelIn);
+			//for finding POS
+			FileInputStream posModelStream = new FileInputStream(posMFile);
+			POSModel model = new POSModel(posModelStream);
+			this.tagger = new POSTaggerME(model);
+			//for parsing
+			parserStream = new FileInputStream(parserMFile);
+			this.parserModel = new ParserModel(parserStream);
+		} catch (FileNotFoundException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.modelsAreInitialized=true;
+	}
+
+	public void testSentDetector(String testSents) throws InvalidFormatException{
+		init();
+		SentenceDetectorME sentenceDetector = new SentenceDetectorME(this.sentenceModel);
+		String[] sentences = sentenceDetector.sentDetect(testSents);
+		for (int i=0;i<sentences.length; i++)
+			System.err.println("sent: "+sentences[i]);
+	}
+
 
 }
