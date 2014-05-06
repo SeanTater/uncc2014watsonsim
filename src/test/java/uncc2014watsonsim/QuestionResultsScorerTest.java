5209e2b1b7d05a88f05f1b720e3181863a966677
Sean Gallagher <stgallag@gmail.com>
2014-05-06 11:39:27 -0400

    Put in a schema. Remove two dead modules

--- src/test/java/uncc2014watsonsim/QuestionResultsScorerTest.java
+++ src/test/java/uncc2014watsonsim/QuestionResultsScorerTest.java
@@ -5,15 +5,13 @@
 
 package uncc2014watsonsim;
 import uncc2014watsonsim.researchers.CombineScores;
-import uncc2014watsonsim.scorers.AllEnginesResultsScorer;
 
 
 public class QuestionResultsScorerTest {
 	
 	public static void main(String[] args) {
 		try {
-			CombineScores q = new AllEnginesResultsScorer();
-			q.initialize();
+			CombineScores q = new CombineScores();
 			System.out.println("scoring: {indri-rank=1, indri-score=-1.582, lucene-rank=1, lucene-score=7.215, google-rank=1} ==> " + 
 					q.score(new double[]{1,-1.582,1,7.215,1}));			
 		} catch (Exception e) {
