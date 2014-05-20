23a00b44f79340d0e238b9c2c064a22a8ba23602
Sean Gallagher <stgallag@gmail.com>
2014-05-20 14:59:42 -0400

    Add PostgreSQL support

--- src/main/java/uncc2014watsonsim/SQLiteDB.java
+++ src/main/java/uncc2014watsonsim/SQLiteDB.java
@@ -11,9 +11,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-
 public class SQLiteDB {
-	private final Connection conn;
+	private Connection conn;
 	private static final Map<String, Connection> connections = new HashMap<String, Connection>();
 	private final Map<String, PreparedStatement> statements = new HashMap<String, PreparedStatement>();
 	private String name;
@@ -34,25 +33,41 @@ public class SQLiteDB {
 			conn = connections.get(name); 
 		} else {
 			try {
-			    // Load the sqlite-JDBC driver using the current class loader
-			    Class.forName("org.sqlite.JDBC");
-				conn = DriverManager.getConnection("jdbc:sqlite:data/" + name + ".db");
-				conn.createStatement().execute("PRAGMA journal_mode = TRUNCATE;");
-				conn.createStatement().execute("PRAGMA synchronous = OFF;");
-
-				// JDBC's SQLite uses autocommit (So commit() is redundant)
-				// Furthermore, close() is a no-op as long as the results are commit()'d
-			} catch(SQLException | ClassNotFoundException e) {
-		       // if the error message is "out of memory", 
-		       // it probably means no database file is found
-		       e.printStackTrace();
-		       throw new RuntimeException("Can't run without a database.");
+				init_postgres();
+			} catch (SQLException | ClassNotFoundException e1) {
+			       e1.printStackTrace();
+			       System.err.println("Failed to connect to postgres; using sqlite.");
+			       try {
+			    	   init_sqlite();
+			       } catch (SQLException | ClassNotFoundException e2) {
+			    	   e2.printStackTrace();
+			    	   throw new RuntimeException("Can't run without a database.");
+			       }
 			}
 			connections.put(name, conn);
 		}
 		if (!sanityCheck()) {
 			System.out.println(String.format("Warning: Database \"%s\" missing or empty. Full texts will come from Indri and Lucene.", name));
 		}
+	}
+	
+	private void init_postgres() throws ClassNotFoundException, SQLException {
+		// Loads a postgresql driver, with no username/password
+	    Class.forName("org.postgresql.Driver");
+		conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/" + name);
+
+	}
+
+	private void init_sqlite() throws ClassNotFoundException, SQLException {
+	    // Load the sqlite-JDBC driver using the current class loader
+	    Class.forName("org.sqlite.JDBC");
+		conn = DriverManager.getConnection("jdbc:sqlite:data/" + name + ".db");
+		conn.createStatement().execute("PRAGMA journal_mode = TRUNCATE;");
+		conn.createStatement().execute("PRAGMA synchronous = OFF;");
+		conn.createStatement().execute("PRAGMA busy_timeout = 30000;");
+
+		// JDBC's SQLite uses autocommit (So commit() is redundant)
+		// Furthermore, close() is a no-op as long as the results are commit()'d
 	}
 	
 	/** Caching proxy for Connection.prepareStatement.
