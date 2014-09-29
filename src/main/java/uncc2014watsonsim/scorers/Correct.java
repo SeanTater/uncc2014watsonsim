be048348fb7a86e4fbb7e51ebb44b3908b8f918e
Sean Gallagher <stgallag@gmail.com>
2014-09-29 14:19:51 -0400

    fix comment

--- src/main/java/uncc2014watsonsim/scorers/Correct.java
+++ src/main/java/uncc2014watsonsim/scorers/Correct.java
@@ -7,7 +7,7 @@ public class Correct extends AnswerScorer {
 	@Override
 	/**
 	 * Generate the target attribute for Machine Learning.
-	 * @returns correctness		0.0 -> correct, >= 2.0 -> incorrect
+	 * @returns correctness		0.0 -> incorrect,  1.0 -> correct
 	 * */
 	public double scoreAnswer(Question q, Answer a) {
 		return a.distance(q.answer) < 2 ? 1 : 0;
