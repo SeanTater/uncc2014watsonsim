8a74b70166d47e144f88b17c273e938dd484dc8d
Sean Gallagher <stgallag@gmail.com>
2015-01-24 14:17:15 -0500

    Keep weka from overwriting arffs again

--- src/main/java/uncc2014watsonsim/researchers/WekaTee.java
+++ src/main/java/uncc2014watsonsim/researchers/WekaTee.java
@@ -19,9 +19,9 @@ import weka.core.converters.Saver;
 
 /** Pipe Answer scores to an ARFF file for Weka */
 public class WekaTee extends Researcher {
-	private final List<double[]> dataset = new ArrayList<>();
-	private ArffSaver saver;
-	private int saved_schema_version = -1;
+	private final static List<double[]> dataset = new ArrayList<>();
+	private static ArffSaver saver;
+	private static int saved_schema_version = -1;
 	
 	
 	// Make every run unique, but overwrite between questions
@@ -41,31 +41,29 @@ public class WekaTee extends Researcher {
 	}
 
 	@Override
-	public synchronized void question(Question q) {
+	/**
+	 * Multithreaded counterpart to dump, which is synchronized
+	 */
+	public void question(Question q) {
+
 		List<double[]> new_entries = new ArrayList<>();
 		for (Answer a : q)
 			new_entries.add(a.scores.clone());
+		dump(new_entries, start_time);
+	}
+	
+	/** File-writing serialized counterpart to question()
+	 * 
+	 * @param new_entries	The new arrays to dump
+	 * @param start_time	The timpstamp of the file to dump to
+	 */
+	private static synchronized void dump(List<double[]> new_entries, Timestamp start_time) {
 		dataset.addAll(new_entries);
 		
-		// When the score changes, rewrite the file.
 		String[] current_schema = Score.latestSchema();
 		try {
 			if (current_schema.length != saved_schema_version) {
-				saved_schema_version = current_schema.length;
-				
-				FastVector attributes = new FastVector();
-				// Answer score names
-				for (String name: current_schema)
-					attributes.addElement(new Attribute(name));
-				Instances data = new Instances("Watsonsim captured question stream", attributes, 0);
-				
-				// Save the results to a file
-				saver = new ArffSaver();
-				saver.setStructure(data);
-				saver.setRetrieval(Saver.INCREMENTAL);
-				saver.setFile(new File("data/weka-log." + start_time + ".arff"));
-				for (double[] row : dataset)
-					saver.writeIncremental(new Instance(1.0, Score.update(row.clone())));
+				dump_from_scratch(current_schema, start_time);
 			} else {
 				// Only do a few quick updates
 				for (double[] row : new_entries)
@@ -75,7 +73,28 @@ public class WekaTee extends Researcher {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to write Weka Log!");
 		}
-
+	}
+	
+	/**
+	 *  When the score changes, rewrite the file.
+	 *  This is really rare in practice, so don't bother optimizing it.
+	 */
+	private static void dump_from_scratch(String[] current_schema, Timestamp start_time) throws IOException {
+		saved_schema_version = current_schema.length;
+		
+		FastVector attributes = new FastVector();
+		// Answer score names
+		for (String name: current_schema)
+			attributes.addElement(new Attribute(name));
+		Instances data = new Instances("Watsonsim captured question stream", attributes, 0);
+		
+		// Save the results to a file
+		saver = new ArffSaver();
+		saver.setStructure(data);
+		saver.setRetrieval(Saver.INCREMENTAL);
+		saver.setFile(new File("data/weka-log." + start_time + ".arff"));
+		for (double[] row : dataset)
+			saver.writeIncremental(new Instance(1.0, Score.update(row.clone())));
 	}
 
 }
