ff0d987aad6f33a78909d06fa20480ff54642e93
Sean Gallagher <stgallag@gmail.com>
2015-02-02 14:47:50 -0500

    Split DetectLAT into two classes

--- src/main/java/uncc2014watsonsim/DetectLAT.java
+++ src/main/java/uncc2014watsonsim/DetectLAT.java
@@ -1,64 +1,12 @@
 package uncc2014watsonsim;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import uncc2014watsonsim.nlp.NLPUtils;
 import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
-import edu.stanford.nlp.util.CoreMap;
 
-import uncc2014watsonsim.Answer;
-import uncc2014watsonsim.Passage;
-import uncc2014watsonsim.Question;
-import uncc2014watsonsim.StringUtils;
-
-/* @author Wlodek
- * @author Sean Gallagher
- * 
+/**
+ * Detect the LAT as the noun in closest proximity to a determiner.
  */
-
 public class DetectLAT {
-	StanfordCoreNLP pipeline;
-	public DetectLAT() {
-		// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution 
-	    Properties props = new Properties();
-	    props.put("annotators", "tokenize, ssplit, pos, parse");
-	    props.put("parse.model", "edu/stanford/nlp/models/srparser/englishSR.ser.gz");
-	    pipeline = new StanfordCoreNLP(props);
-	}
-	
-	public List<Tree> parseToTrees(String text) {
-	    
-	    // create an empty Annotation just with the given text
-	    Annotation document = new Annotation(text);
-	    
-	    // run all Annotators on this text
-	    pipeline.annotate(document);
-	    
-	    // these are all the sentences in this document
-	    // a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-	    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-	    List<Tree> trees = new ArrayList<>();
-	    
-	    for(CoreMap sentence: sentences) {
-	      // this is the parse tree of the current sentence
-	      trees.add(sentence.get(TreeAnnotation.class));
-	    }
-	    return trees;
-	}
 	
 	/**
 	 * Intermediate results from LAT detection
@@ -105,7 +53,7 @@ public class DetectLAT {
 	}
 	
 	public String simpleFetchLAT(String s) {
-		for (Tree t : parseToTrees(s)) {
+		for (Tree t : NLPUtils.parseToTrees(s)) {
 			LATStatus lat = simpleFetchLAT(t);
 			if (lat.nn != null) return treeAsString(lat.nn);
 		}
