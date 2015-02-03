4bf7da0a8803b4677a1d3765680563851068946d
Sean Gallagher <stgallag@gmail.com>
2015-02-03 13:29:44 -0500

    forgot to add the Trees module in git. Removing the scorer models for now. There are too many to commit them all.

--- src/main/java/uncc2014watsonsim/researchers/CombineScores.java
+++ src/main/java/uncc2014watsonsim/researchers/CombineScores.java
@@ -110,7 +110,7 @@ public class CombineScores extends Researcher {
 	public double score(double[] attributesValues) throws Exception {
 		Instance inst = new Instance(1, attributesValues);
 		inst.setDataset(qResultsDataset);
-		//System.out.println(qResultsDataset.classAttribute().value((int)scorerModel.classifyInstance(inst)));
+		//System.out.println(qResultsDataset.classAttribute().value((int)scorerM	odel.classifyInstance(inst)));
 		return scorerModel.distributionForInstance(inst)[1];
 		//return scorerModel.classifyInstance(inst);
 	}
