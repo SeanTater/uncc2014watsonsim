65efbe03fad7e0f2d11085a660a1574db017b69b
Sean Gallagher <stgallag@gmail.com>
2015-02-11 09:43:23 -0500

    Adding a query for extracting synonyms from wikipedia links

---
+++ src/main/java/uncc2014watsonsim/nlp/Synonyms.java
@@ -0,0 +1,11 @@
+package uncc2014watsonsim.nlp;
+
+public class Synonyms {
+/*
+ * select count(*), trim(both ' .-’`''' from lower(target)) as name
+ * 	from wiki_links
+ * 	where link in ('author','Author', 'writer', 'Writer', 'novelist', 'Novelist')
+ *  group by name having count(*) > 1 
+ *  order by count(*) desc;
+ */
+}
