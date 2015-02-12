09400e157da5f4171aa891e8f7060f33612e96b7
Sean Gallagher <stgallag@gmail.com>
2015-02-12 15:51:31 -0500

    Added a LAT checking scorer

--- src/main/java/uncc2014watsonsim/researchers/TagLAT.java
+++ src/main/java/uncc2014watsonsim/researchers/TagLAT.java
@@ -7,16 +7,47 @@ import uncc2014watsonsim.Answer;
 import uncc2014watsonsim.Question;
 
 import com.hp.hpl.jena.query.*;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
+import com.hp.hpl.jena.tdb.TDBFactory;
 
 import static uncc2014watsonsim.StringUtils.sanitize;
 
 public class TagLAT extends Researcher {
 	private final String sparql_url;
 	private final Random gen = new Random();
+	private final Dataset dataset;
+	private final Model model;
+	private final Query query = QueryFactory.create("PREFIX : <http://dbpedia.org/resource/>"
+			+ "PREFIX dbo: <http://dbpedia.org/ontology/>"
+			+ "PREFIX dbpedia2: <http://dbpedia.org/property/>"
+			+ "PREFIX dbpedia: <http://dbpedia.org/>"
+			+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+			+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
+			+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
+			+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
+			+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+			+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+			+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
+			+ "select distinct ?typename where {"
+			+ "?thing rdfs:label ?thingname."
+			+ "?thingname <bif:contains> ?name ."
+			//		+ "UNION"
+			//+ "{ ?thing foaf:name  ?thingname."	
+			//+ "?thingname <bif:contains> \"'"+ target_name + "'\". }"
+					+ ""
+			+ "?thing rdf:type ?type."
+			+ "?type rdfs:label ?typename."
+			+ "FILTER ("
+				+ "langMatches(lang(?typename), 'EN')"
+			+ ")} limit 10");
+	
 	public TagLAT(String sparql_url) {
 		this.sparql_url = sparql_url;
+		dataset = TDBFactory.createDataset("data/jena-tdb");
+		dataset.begin(ReadWrite.READ);
+		model = dataset.getDefaultModel();
 	}
 	
 	/**
@@ -66,35 +97,7 @@ select distinct ?typename where {
   langMatches(lang(?typename), 'EN')
 )} limit 10"
 		 */
-		QueryExecution qe = QueryExecutionFactory.sparqlService(
-				sparql_url,
-				// There are several possible servers
-				//"http://dbpedia.org/sparql",
-				//"http://pasky.or.cz:3030/dbpedia/query",
-				"PREFIX : <http://dbpedia.org/resource/>"
-				+ "PREFIX dbo: <http://dbpedia.org/ontology/>"
-				+ "PREFIX dbpedia2: <http://dbpedia.org/property/>"
-				+ "PREFIX dbpedia: <http://dbpedia.org/>"
-				+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
-				+ "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"
-				+ "PREFIX owl: <http://www.w3.org/2002/07/owl#>"
-				+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"
-				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-				+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
-				+ "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>"
-				+ "select distinct ?typename where {"
-				+ "?thing rdfs:label ?thingname."
-				+ "?thingname <bif:contains> \"'"+ target_name + "'\"."
-				//		+ "UNION"
-				//+ "{ ?thing foaf:name  ?thingname."	
-				//+ "?thingname <bif:contains> \"'"+ target_name + "'\". }"
-						+ ""
-				+ "?thing rdf:type ?type."
-				+ "?type rdfs:label ?typename."
-				+ "FILTER ("
-					+ "langMatches(lang(?typename), 'EN')"
-				+ ")} limit 10");
-		qe.setTimeout(5000);
+		
 		
 		/*
 		 * ABOUT THE QUERY
@@ -119,9 +122,12 @@ select distinct ?typename where {
 		 * 
 		 */
 		List<String> types = new ArrayList<>();
-		try {
+		QuerySolutionMap params = new QuerySolutionMap();
+		params.add("namecontent", model.createTypedLiteral(candidate_text));
+		try (QueryExecution qe = QueryExecutionFactory.create(query, model)) {
+			qe.setInitialBinding(params);
+			qe.setTimeout(5000);
 			ResultSet rs = qe.execSelect();
-
 			while (rs.hasNext()) {
 				QuerySolution s = rs.next();
 				RDFNode node = s.get("?typename");
@@ -129,9 +135,6 @@ select distinct ?typename where {
 				else if (node.isLiteral()) types.add(node.asLiteral().getLexicalForm().toLowerCase());
 				else if (node.isResource()) types.add(node.asResource().getLocalName().toLowerCase());
 			}
-		} catch (QueryExceptionHTTP h) {
-			System.err.println("LAT timeout on " + target_name);
-			if (retries>0) return types(candidate_text, retries-1);
 		}
 
 		System.out.println(target_name + ": " + types);
