7394eb262f7540d78b9c1ad18dc9c739abe98e6f
Sean Gallagher <stgallag@gmail.com>
2015-03-06 17:41:35 -0500

    Removing String annotations until we have a clear need for them.

--- src/main/java/uncc2014watsonsim/qAnalysis/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Classes used in analyzing the question/category in order to determine what
- *  it is asking for and how best to approach answering it. 
- * @author Ken Overholt
- */
-package uncc2014watsonsim.qAnalysis;
