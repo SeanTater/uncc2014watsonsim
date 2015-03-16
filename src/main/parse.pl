5687b050bb4b00fbf308dd36a4765adb6934da72
Sean Gallagher <stgallag@gmail.com>
2015-03-16 11:35:19 -0400

    Create new Prolog based rules rather than Jena

---
+++ src/main/parse.pl
@@ -0,0 +1,20 @@
+is_type_statement(A, B) :-
+    (nsubj(A, B); nsubj(B, A)),
+    (cop(A, _); cop(B, _)).
+
+/* A plain type */
+type_a(Name, Type) :-
+    is_type_statement(Name, Type),
+    det(Type, _).
+
+/* A type with subject conjunctions */
+type_b(Name, Type) :-
+    type_a(Name, Type) ;
+    (type_a(AnotherName, Type),
+     conj_and(AnotherName, Name)).
+
+/* A type with subject or type conjunctions */
+type_c(Name, Type) :-
+    type_b(Name, Type) ;
+    (type_b(Name, AnotherType),
+     conj_and(AnotherType, Type)).
