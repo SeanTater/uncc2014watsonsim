9c922e2bc9ad12861373859c7cd679c2093d406f
Sean Gallagher <stgallag@gmail.com>
2015-05-02 18:49:32 -0400

    Try to make the web interface more lively

--- src/main/java/edu/uncc/cs/watsonsim/WebFrontend.java
+++ src/main/java/edu/uncc/cs/watsonsim/WebFrontend.java
@@ -1,30 +1,49 @@
 package edu.uncc.cs.watsonsim;
 import static spark.Spark.*;
 
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.apache.log4j.WriterAppender;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.Logger;
 
 import spark.*;
 
 public class WebFrontend {
+	
+	private static final ConcurrentHashMap<Integer, DefaultPipeline>
+		ongoing_pipelines = new ConcurrentHashMap<>();
+	private static final Random rand = new Random();
 
 	public static void main(String[] args) {
 		
 		externalStaticFileLocation("public");
-		get(new Route("/ask") {
-			@Override
-			public Object handle(Request request, Response response) {
-	    		Question question = new Question(request.queryParams("query"));
-	    		List<Answer> answers = new DefaultPipeline().ask(question);
-		        
-	    		String output = "";
-		        // Print out a simple one-line summary of each answer
-		        for (Answer r: answers) {
-		        	output += r.toJSON() + ",";
-		        }
-		        response.type("application/json");
-		        return String.format("{\"answers\": [%s]}", output.substring(0, output.length() - 1));
-			}
+		get("/ask", (Request request, Response response) -> {
+    		Question question = new Question(request.queryParams("query"));
+    		Logger l = Logger.getLogger(WebFrontend.class);
+    		OutputStream st = response.raw().getOutputStream();
+    		l.addAppender(
+    				new WriterAppender(
+    						new SimpleLayout(),
+    						st));
+    		List<Answer> answers = new DefaultPipeline().ask(question);
+	        
+    		StringBuilder output = new StringBuilder();
+	        // Throw whole summaries of the data at the client
+	        for (Answer r: answers) {
+	        	output.append(r.toJSON());
+	        	output.append(',');
+	        }
+	        
+	        
+	        response.type("application/json");
+	        return String.format("{\"id\", {\"answers\": [%s]}", output.substring(0, output.length() - 1));
 		});
+		
+		
 
 	}
 
