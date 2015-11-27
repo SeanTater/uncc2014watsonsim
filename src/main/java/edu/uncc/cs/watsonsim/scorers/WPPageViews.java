9ef3d0caa5013b16e3421a396da0970ce201be9d
Sean Gallagher <stgallag@gmail.com>
2015-11-26 20:37:12 -0500

    Include pageviews again now that the tables are small and fast

--- src/main/java/edu/uncc/cs/watsonsim/scorers/WPPageViews.java
+++ src/main/java/edu/uncc/cs/watsonsim/scorers/WPPageViews.java
@@ -7,35 +7,38 @@ import java.util.HashMap;
 import edu.uncc.cs.watsonsim.Answer;
 import edu.uncc.cs.watsonsim.Environment;
 import edu.uncc.cs.watsonsim.Question;
+import edu.uncc.cs.watsonsim.nlp.ApproxStringIntMap;
 
 public class WPPageViews extends AnswerScorer {
-	private static final HashMap<String, Integer> pageviews
-		= new HashMap<>(5000000);
+	private static ApproxStringIntMap pageviews = new ApproxStringIntMap(null);
 	
 	public WPPageViews(Environment env) {
-		//load(env);
+		load(env);
 	}
 	
 	private static synchronized void load(Environment env) {
 		if (pageviews.isEmpty()) {
+			int collisions = 0;
 			try {
 				ResultSet res = env.db.prep(
 						"SELECT title, page_views FROM page_views;")
 						.executeQuery();
 				while (res.next()) {
-					pageviews.merge(res.getString(1).toLowerCase(), res.getInt(1), Integer::sum);
+					collisions += pageviews.containsKey(res.getString(1).toLowerCase()) ? 1 : 0;
+					pageviews.put(res.getString(1).toLowerCase(), res.getInt(2));
 				}
 			} catch (SQLException e) {
 				// at worst give 0s
 				e.printStackTrace();
 			}
-			System.out.println("Loaded " + pageviews.size() + " pageview stats");
+			System.out.println("Loaded view data about " + pageviews.size() + " pages "
+					+ "(" + collisions + " collisions)");
 		}
 	}
 
 	@Override
 	public double scoreAnswer(Question q, Answer a) {
-		return pageviews.getOrDefault(a.text.toLowerCase(), 0);
+		return pageviews.get(a.toString().toLowerCase());
 	}
 	
 }
